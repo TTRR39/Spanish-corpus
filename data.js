69a2aee0ef55e3c5a201a8189232e674ba9d7a18
TTRR39 <chengdutian33@gmail.com>
2026-03-13 01:52:51 +0000

    Create data.js

---
+++ data.js
@@ -0,0 +1,27 @@
+const corpus = [
+
+{
+source: "chat_Bris",
+date: "2026-03-10",
+text: `No se como explicarte jaja, incluso para mi es dificil.`,
+translation: "I don't know how to explain it toyou, it's hard even for me.",
+notes: "incluso para mi, even for me"
+},
+
+{
+source: "chat_Bris",
+date: "2026-03-10",
+text: `Claro no hay problema!`,
+translation: "Of course, no problem!",
+notes: "claro = of course"
+},
+
+{
+source: "chat_Destello",
+date: "2026-02-20",
+text: `Solo la ultima pieza de la Torre,mas alta,pero faltan otras partes.`,
+translation: "Only the last piece of the Torre (is added), it's taller but other parts remain to be built.",
+notes: "flatar, remain"
+}
+
+];
