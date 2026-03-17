beb7d25fe1629b5ff8c40aed8bb72e273193d2f6
TTRR39 <chengdutian33@gmail.com>
2026-03-17 00:58:04 +0000

    Update data.js

--- data.js
+++ data.js
@@ -17,6 +17,55 @@ notes: "claro = of course"
 },
 
 {
+source: "chat_Bris",
+date: "2026-03-16",
+text: `Esos chismes yo no tengo, yo tengo por las tareas`,
+translation: "I don't have this kind of gossips, mine are about homeworks",
+notes: "chisme=gossip, por... about"
+},
+
+  {
+source: "chat_Bris",
+date: "2026-03-16",
+text: `Que tengo miedo, tengo miedo!`,
+translation: "I'm so scary, so scary",
+notes: "que+... emphasize"
+},
+
+  {
+source: "chat_Bris",
+date: "2026-03-16",
+text: `Pero no me importa, soy mas bueno en ingles`,
+translation: "english is ok, i have a better hang of it",
+notes: "no me importa"
+},
+
+
+    {
+source: "chat_Bris",
+date: "2026-03-16",
+text: `Porque es como te que 'wow tu sabes hablar chino!'`,
+translation: "IT's like people will say to you wow you can speak chinese",
+notes: "como te que"
+},
+
+  {
+source: "chat_Bris",
+date: "2026-03-16",
+text: `Hoy es una fecha importante en Mexico, es natalicio de Benito Juarez, el presidente de Mexico.`,
+translation: "Today is an important date in Mexico. The birthday of Benito Juarez, president of Mexico.",
+notes: " 3.16-- natalicio de Benito Juarez, a famous president in 19C, honored among the greatest of Mexicans."
+},
+
+    {
+source: "chat_Bris",
+date: "2026-03-16",
+text: `Es zafado!`,
+translation: "It's crazy!",
+notes: "zafado, from zafar to loosen, from Arabic azahah"
+},
+  
+{
 source: "chat_Destello",
 date: "2026-02-20",
 text: `Solo la ultima pieza de la Torre,mas alta,pero faltan otras partes.`,
